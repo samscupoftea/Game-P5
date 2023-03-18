da85232525e8ad592c8f2bb193fb043e552a5a50
sam hockaday <samuel.hockaday@gmail.com>
2023-03-18 21:02:12 +0000

    Added JS code that creates random gaps.

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let block = document.getElementById("block");
 let hole = document.getElementById("hole");
 // CREATE EVENT LISTENER ON THE "HOLE" ELEMENT THAT RUNS AFTER EVERY ANIMATION. 
 hole.addEventListener('animationiteration' , () => {
+    let random = ((Math.random()*300)+150);
+    hole.style.top = random + "px";
+
 
 });
