821f818084acb22527b5753735c2371395bd4031
sam hockaday <samuel.hockaday@gmail.com>
2023-03-19 21:19:23 +0000

    JS alterations.

--- script.js
+++ script.js
@@ -39,26 +39,25 @@
 //
 // Re-done 
 
-
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = 0;
-var counter = 0;
+const block = document.getElementById("block");
+const hole = document.getElementById("hole");
+const character = document.getElementById("character");
+let jumping = 0;
+let counter = 0;
 
 hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random()*300)+150);
+    const random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
     counter++;
 });
 setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
         character.style.top = (characterTop+3)+"px";
     }
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var cTop = -(500-characterTop);
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    const cTop = -(500-characterTop);
     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
         alert("Game over. Score: "+(counter-1));
         character.style.top = 100 + "px";
@@ -69,8 +68,8 @@ setInterval(function(){
 function jump(){
     jumping = 1;
     let jumpCount = 0;
-    var jumpInterval = setInterval(function(){
-        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    const jumpInterval = setInterval(function(){
+        const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         if((characterTop>6)&&(jumpCount<15)){
             character.style.top = (characterTop-5)+"px";
         }
